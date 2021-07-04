a93d15b0ddd21d085f896bc950f6bbb743f9098d
Khanh <khanhnpgdd191004@fpt.edu.vn>
2021-07-04 13:26:27 +0700

    finish

--- login1.php
+++ login1.php
@@ -32,7 +32,8 @@ table {
  <th colspan="2" align="center">Operation</th>
  </tr>
 <?php
-echo '<p> ATN Shop 1 </p>';
+echo '<p style =" text-align: center;"> <b> ATN Shop 1 </b> </p>';
+
 $host_heroku = "ec2-54-158-232-223.compute-1.amazonaws.com";
 $db_heroku = "devkq9p8tndv0m";
 $user_heroku = "ecfkdwhillmjlv";
